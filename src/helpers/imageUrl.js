af9184e82d32b9aa872ffef20e79b2fdff539c86
Ahulo Sunday <ahulosunday@yahoo.com>
2023-09-10 21:08:40 +0100

    last

--- src/helpers/imageUrl.js
+++ src/helpers/imageUrl.js
@@ -1,3 +1,3 @@
-   const baseURLStatic = "http://localhost:6001/upload/";
-   //const baseURLStatic = "https://api-2f2e.onrender.com/upload/";
+   //const baseURLStatic = "http://localhost:6001/upload/";
+   const baseURLStatic = "https://api-2f2e.onrender.com/upload/";
  export default baseURLStatic;
