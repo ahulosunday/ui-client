fde9d3f2a9631ea781b0471df939c6cd8bbe2135
Ahulo Sunday <ahulosunday@yahoo.com>
2023-09-18 14:56:10 +0100

    ok

--- src/pages/user-rrr/generatePayment.jsx
+++ src/pages/user-rrr/generatePayment.jsx
@@ -16,8 +16,12 @@ import { formatCurreny } from '../../components/formatCurrency';
 import CheckBoxIcon from '@mui/icons-material/CheckBox';
 import ClearIcon from '@mui/icons-material/Clear';
 import AddTaskIcon from '@mui/icons-material/AddTask';
+import moment from 'moment';
+import { Alert } from '@mui/material';
 const GeneratePayment = ()=>{
 const [expanded, setExpanded] = React.useState(false);
+ const [checked, setChecked] = React.useState([])
+ const [err, setErr] = React.useState('')
  const [getrrr, setGetrrr] = React.useState([]);
     const [page, setPage]= React.useState(1)
     const [data, setData] = React.useState([])
@@ -34,9 +38,7 @@ const [expanded, setExpanded] = React.useState(false);
         })
 
   }
-  const handleChange = (panel) => (event, isExpanded) => {
-    setExpanded(isExpanded ? panel : false);
-  };
+
   const getCount = async (user_rrrId)=>{
     try{
       const count = await app.get(`/codes/${user_rrrId}/code/rrr/`)
@@ -47,12 +49,8 @@ const [expanded, setExpanded] = React.useState(false);
     }
     
   }
-  React.useEffect(()=>{
-    if(!(permissions.indexOf("VIEW_RRR") > -1) ){
-        navigate('/')
-    }
 
-const loadItem = async e =>{
+  const loadItem = async e =>{
  try{
          
         await app.get(`/rrr/not/activate/0/1/1/`).then(res=>{
@@ -68,24 +66,55 @@ const loadItem = async e =>{
         }
         
     }
-   
-         loadItem()
+  React.useEffect(()=>{
+    if(!(permissions.indexOf("VIEW_RRR") > -1) ){
+        navigate('/')
+    }
+    loadItem()
          
       
   }, [currentUser, permissions, navigate])
 
-const getData = async ()=>{
+
+     
+
+const getData = async (e)=>{
+  e.preventDefault()
+  try{
+  var plist = document.getElementById('plist').value;
+   if(plist.length !== 0){
+    var activated_date = formatDate(new Date('2023-09-18'))//document.getElementById('activated_date').value;
+            var oResponse = [];
+            oResponse = plist.split(',')
+           
 var arr = [];
-const oFullResponse = ['u', 'v', 'w', 'x']
-var len = oFullResponse.length;
+var len = oResponse.length;
+
 for (var i = 0; i < len; i++) {
+  const dataLog = await app.get(`/user-rrr/${oResponse[i]}/`)
+       let  expired_date= moment(Date.parse(activated_date) + ((dataLog.data.duration * 1000 * 60 * 60 * 24))).format('YYYY-MM-DD')
     arr.push({
-        id: oFullResponse[i],
-        name: 'name',
-        date: 'date'
-    });
+        id: oResponse[i],
+        activatedby: currentUser.id,
+        activated_date: activated_date,
+        expired_date: expired_date
+    }); 
+ 
 }
-console.log(arr)
+await app.put('/user-rrr/', arr).then(res=>{
+  loadItem()
+ setErr(<Alert color='secondary'>Transaction completed successfully</Alert>)
+})
+.catch(err=>{
+
+})
+
+}
+
+  }
+  catch(err){
+
+  }
 }
    const [search, setSearch] = React.useState('');
   const handleSearch = (event) => {
@@ -99,6 +128,17 @@ const datas = {
   ),
 };
  var sum = 0;
+
+ const handleCheckAllChange = (e) => {
+          setChecked( e.target.checked ? datas.nodes.map((c) => {
+            if(getCount(c.id) === c.maxNumber)
+            return c.id
+            }) : []);
+        };
+
+        const handlePermissionChange = (e, c) => {
+          setChecked((prevChecked) => e.target.checked ? [...prevChecked, c.id]: prevChecked.filter((item) => item !== c.id));
+        };
   return (
  <CRow >
 <CCol xs={12} >
@@ -113,11 +153,13 @@ const datas = {
       </CInputGroup>
             <DocsExample add="Payment List">
        
-  
+               <p>{err}</p>
           <CTable style={{fontSize:'12px'}} align="middle" responsive>
           <CTableHead>
           <CTableRow>
-          <CTableDataCell><input type='checkbox' name='id' /></CTableDataCell>
+          <CTableDataCell><input type='checkbox' id="selectAll"
+                checked={checked.length === datas.nodes.length}
+                onChange={handleCheckAllChange} /></CTableDataCell>
           <CTableDataCell>RRR NUMBER</CTableDataCell>
           <CTableDataCell>AUTH. CODE</CTableDataCell>
           <CTableDataCell>AMOUNT</CTableDataCell>
@@ -135,8 +177,13 @@ const datas = {
 
             return(
             <CTableRow key={item.id}>
-             <CTableDataCell>{getCount(item.id) === item.maxNumber ? <input type='checkbox' name='id' id={item.id} />: null}</CTableDataCell>
-       <CTableDataCell><Link title='View the list of enrolees under this payment' to={`/user-rrr/dependants`} state={item.id}>{item.rrr_number}</Link></CTableDataCell>
+             <CTableDataCell>{getCount(item.id) !== item.maxNumber ? <input type="checkbox" id={item.id}
+                checked={checked.includes(item.id)}
+                  onChange={(e) => handlePermissionChange(e, item)}  
+                  value={item.id}
+                  name="ck"/>: null}</CTableDataCell>
+       <CTableDataCell>
+       <Link title='View the list of enrolees under this payment' to={`/user-rrr/dependants`} state={item.id}>{item.rrr_number}</Link></CTableDataCell>
         <CTableDataCell>{item.authNumber}</CTableDataCell>
           <CTableDataCell>{formatCurreny.format(item.amount)}</CTableDataCell>
           <CTableDataCell>{item.maxNumber}</CTableDataCell>
@@ -151,8 +198,9 @@ const datas = {
     
         <AccordionDetails>
           <Typography>
+          <CFormInput type="hidden" name="plist" id="plist" value={checked.join(",")} />
            Sum: {formatCurreny.format(sum)} 
-           <p style={{textAlign:'right'}}><Link title='Activate the selected record(s)' to={'/user-rrr/'}><AddTaskIcon /> Activate</Link></p>
+           <p style={{textAlign:'right'}}><Link title='Activate the selected record(s)' onClick={getData}><AddTaskIcon /> Activate</Link></p>
           </Typography>
         </AccordionDetails>
       
